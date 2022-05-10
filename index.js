d884b4fb8189283efe9b7ab6242e4f99d30e28d8
jkalavadiya <94731258+jkalavadiya@users.noreply.github.com>
2022-05-10 13:00:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,234 @@
+
+let mydata=
+{
+	"data": {
+		"columns": {
+			"last_update": "Last Update",
+            "email": "Email",
+            "address": "Address",
+            "labelName": "Label",
+            "name": "Name",
+            "customer_id": "DEB-Auto",
+            "id": "Id",
+		},
+		"searchableColumns": ["customer_id", "name", "address"],
+		"sortableColumns": ["id", "customer_id", "name", "address", "email", "last_update"],
+		"organizations": [{
+			"id": 731,
+			"customer_id": "1427",
+			"name": "Test ajay",
+			"address": "PosBill GmbH, Brunnengasse, Kehlbach, Germany",
+			"email": null,
+			"last_update": "2022-03-30T07:11:08.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 867,
+			"bankaccount_info2": null
+		}, {
+			"id": 763,
+			"customer_id": "1417",
+			"name": "test-QA1",
+			"address": "German Experts Car Maintenance - Abu Dhabi - United Arab Emirates",
+			"email": null,
+			"last_update": "2022-03-30T07:02:32.000000Z",
+			"labelName": "",
+			"labelColor": null,
+			"orgId": 883,
+			"bankaccount_info2": null
+		}, {
+			"id": 755,
+			"customer_id": "1418",
+			"name": "Atest4",
+			"address": "Gera, Germany",
+			"email": null,
+			"last_update": "2022-03-30T07:02:06.000000Z",
+			"labelName": "",
+			"labelColor": null,
+			"orgId": 879,
+			"bankaccount_info2": null
+		}, {
+			"id": 751,
+			"customer_id": "1419",
+			"name": "Atest2",
+			"address": null,
+			"email": null,
+			"last_update": "2022-03-30T07:01:46.000000Z",
+			"labelName": "",
+			"labelColor": null,
+			"orgId": 877,
+			"bankaccount_info2": null
+		}, {
+			"id": 730,
+			"customer_id": "1426",
+			"name": "test sevDesk m1",
+			"address": "PosBill GmbH, Brunnengasse, Kehlbach, Germany",
+			"email": null,
+			"last_update": "2022-03-30T07:00:42.000000Z",
+			"labelName": "",
+			"labelColor": null,
+			"orgId": 866,
+			"bankaccount_info2": null
+		}, {
+			"id": 747,
+			"customer_id": "1432",
+			"name": "test-shivani3",
+			"address": "Germany",
+			"email": "shivani@test.com",
+			"last_update": "2022-03-30T06:59:58.000000Z",
+			"labelName": "",
+			"labelColor": null,
+			"orgId": 875,
+			"bankaccount_info2": null
+		}, {
+			"id": 862,
+			"customer_id": "1485",
+			"name": "Gellert Imbiss",
+			"address": "Quellenstrasse 77 , Wien, 1100, AT Avusturya",
+			"email": "sales@gellertimbiss.com",
+			"last_update": "2022-03-02T09:14:07.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 947,
+			"bankaccount_info2": null
+		}, {
+			"id": 863,
+			"customer_id": "1486",
+			"name": "test1.0",
+			"address": "",
+			"email": "ngvdungit2@maildrop.cc",
+			"last_update": "2022-03-09T09:24:57.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 948,
+			"bankaccount_info2": null
+		}, {
+			"id": 859,
+			"customer_id": "1482",
+			"name": "R5",
+			"address": "Sinabelkirchen 8, Sinabelkirchen, 8261,",
+			"email": "weberhofer.andreas@gmail.com",
+			"last_update": "2022-03-02T07:08:32.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 943,
+			"bankaccount_info2": null
+		}, {
+			"id": 860,
+			"customer_id": "1483",
+			"name": "Onassi",
+			"address": "Langemarckstr. 171, Bremen, 28199, DE Deutschland",
+			"email": "lpoetsch@rea.de",
+			"last_update": "2022-03-02T07:30:34.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 945,
+			"bankaccount_info2": null
+		}, {
+			"id": 861,
+			"customer_id": "1484",
+			"name": "nguyen",
+			"address": "",
+			"email": "ngvdung90it@maildrop.cc",
+			"last_update": "2022-03-02T08:46:46.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 946,
+			"bankaccount_info2": null
+		}, {
+			"id": 856,
+			"customer_id": "1479",
+			"name": "Ruchita 6291",
+			"address": "3B, Innenstadt, 14473, Germany",
+			"email": "ruchitas@scaletech.xyz",
+			"last_update": "2022-03-02T06:51:40.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 940,
+			"bankaccount_info2": null
+		}, {
+			"id": 857,
+			"customer_id": "1480",
+			"name": "Alois Geyer",
+			"address": "Mitterfeldgasse 21, Ziersdorf, 3710,",
+			"email": "aloisgeyer@gmx.at",
+			"last_update": "2022-03-02T07:05:46.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 941,
+			"bankaccount_info2": null
+		}, {
+			"id": 858,
+			"customer_id": "1481",
+			"name": "Alois Geyer",
+			"address": "Mitterfeldgasse 21, Ziersdorf, 3710,",
+			"email": "aloisgeyer@gmx.at",
+			"last_update": "2022-03-02T07:07:31.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 942,
+			"bankaccount_info2": null
+		}, {
+			"id": 849,
+			"customer_id": "1475",
+			"name": "R1",
+			"address": "",
+			"email": "",
+			"last_update": "2022-03-01T12:29:01.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 936,
+			"bankaccount_info2": null
+		}]
+	},
+	"code": 200
+}
+
+
+
+// function loaddata(items){
+//     let table=document.getElementById("mytable");
+//     console.log("ttt",items)
+    
+//     items.data.columns.forEach((item) => {
+
+//         let columns=table.innerColumn();
+//         columns.insertCell(0).innerHTML=item.id;
+//         columns.insertCell(1).innerHTML=item.customer_id;
+
+//         // columns.insertCell(4).innerHTML=item.Name;
+
+
+//     });
+// }
+// loaddata(mydata);
+
+let arr=Object.keys(mydata.data.columns);
+console.log(arr);
+
+
+function insertdata(items){
+    let table= document.getElementById("mytable");
+    let tablecol=document.getElementById("tablecol");
+    let headerrow=table.insertRow();
+
+
+    const columns = items.data.columns;
+    // console.log(columns );
+    // console.log(columns.address);
+    // console.log(columns["address"]);
+
+    for(let i=0;i<arr.length;i++){
+		headerrow.insertCell(0).innerHTML=columns[arr[i]];
+    }
+
+    items.data.organizations.forEach((item) => {
+        let row=table.insertRow();
+
+        for(let i=0;i<arr.length;i++){
+
+            row.insertCell(0).innerHTML=item[arr[i]];
+        }
+
+    });
+}
+insertdata(mydata);
---
+++ main.js
@@ -0,0 +1,466 @@
+let mydata=
+{
+	"data": {
+		"columns": {
+			"last_update": "Last Update",
+            "email": "Email",
+            "address": "Address",
+            "labelName": "Label",
+            "name": "Name",
+            "customer_id": "DEB-Auto",
+            "id": "Id",
+		},
+		"searchableColumns": ["customer_id", "name", "address"],
+		"sortableColumns": ["id", "customer_id", "name", "address", "email", "last_update"],
+		"organizations": [{
+			"id": 731,
+			"customer_id": "1427",
+			"name": "Test ajay",
+			"address": "PosBill GmbH, Brunnengasse, Kehlbach, Germany",
+			"email": null,
+			"last_update": "2022-03-30T07:11:08.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 867,
+			"bankaccount_info2": null
+		}, {
+			"id": 763,
+			"customer_id": "1417",
+			"name": "test-QA1",
+			"address": "German Experts Car Maintenance - Abu Dhabi - United Arab Emirates",
+			"email": null,
+			"last_update": "2022-03-30T07:02:32.000000Z",
+			"labelName": "",
+			"labelColor": null,
+			"orgId": 883,
+			"bankaccount_info2": null
+		}, {
+			"id": 755,
+			"customer_id": "1418",
+			"name": "Atest4",
+			"address": "Gera, Germany",
+			"email": null,
+			"last_update": "2022-03-30T07:02:06.000000Z",
+			"labelName": "",
+			"labelColor": null,
+			"orgId": 879,
+			"bankaccount_info2": null
+		}, {
+			"id": 751,
+			"customer_id": "1419",
+			"name": "Atest2",
+			"address": null,
+			"email": null,
+			"last_update": "2022-03-30T07:01:46.000000Z",
+			"labelName": "",
+			"labelColor": null,
+			"orgId": 877,
+			"bankaccount_info2": null
+		}, {
+			"id": 730,
+			"customer_id": "1426",
+			"name": "test sevDesk m1",
+			"address": "PosBill GmbH, Brunnengasse, Kehlbach, Germany",
+			"email": null,
+			"last_update": "2022-03-30T07:00:42.000000Z",
+			"labelName": "",
+			"labelColor": null,
+			"orgId": 866,
+			"bankaccount_info2": null
+		}, {
+			"id": 747,
+			"customer_id": "1432",
+			"name": "test-shivani3",
+			"address": "Germany",
+			"email": "shivani@test.com",
+			"last_update": "2022-03-30T06:59:58.000000Z",
+			"labelName": "",
+			"labelColor": null,
+			"orgId": 875,
+			"bankaccount_info2": null
+		}, {
+			"id": 862,
+			"customer_id": "1485",
+			"name": "Gellert Imbiss",
+			"address": "Quellenstrasse 77 , Wien, 1100, AT Avusturya",
+			"email": "sales@gellertimbiss.com",
+			"last_update": "2022-03-02T09:14:07.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 947,
+			"bankaccount_info2": null
+		}, {
+			"id": 863,
+			"customer_id": "1486",
+			"name": "test1.0",
+			"address": "",
+			"email": "ngvdungit2@maildrop.cc",
+			"last_update": "2022-03-09T09:24:57.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 948,
+			"bankaccount_info2": null
+		}, {
+			"id": 859,
+			"customer_id": "1482",
+			"name": "R5",
+			"address": "Sinabelkirchen 8, Sinabelkirchen, 8261,",
+			"email": "weberhofer.andreas@gmail.com",
+			"last_update": "2022-03-02T07:08:32.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 943,
+			"bankaccount_info2": null
+		}, {
+			"id": 860,
+			"customer_id": "1483",
+			"name": "Onassi",
+			"lastname":'Janvi',
+			"address": "Langemarckstr. 171, Bremen, 28199, DE Deutschland",
+			"email": "lpoetsch@rea.de",
+			"last_update": "2022-03-02T07:30:34.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",	
+			"orgId": 945,
+			"bankaccount_info2": null
+		}, {
+			"id": 861,
+			"customer_id": "1484",
+			"name": "nguyen",
+			"address": "",
+			"email": "ngvdung90it@maildrop.cc",
+			"last_update": "2022-03-02T08:46:46.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 946,
+			"bankaccount_info2": null
+		}, {
+			"id": 856,
+			"customer_id": "1479",
+			"name": "Ruchita 6291",
+			"address": "3B, Innenstadt, 14473, Germany",
+			"email": "ruchitas@scaletech.xyz",
+			"last_update": "2022-03-02T06:51:40.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 940,
+			"bankaccount_info2": null
+		}, {
+			"id": 857,
+			"customer_id": "1480",
+			"name": "Alois Geyer",
+			"address": "Mitterfeldgasse 21, Ziersdorf, 3710,",
+			"email": "aloisgeyer@gmx.at",
+			"last_update": "2022-03-02T07:05:46.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 941,
+			"bankaccount_info2": null
+		}, {
+			"id": 858,
+			"customer_id": "1481",
+			"name": "Alois Geyer",
+			"address": "Mitterfeldgasse 21, Ziersdorf, 3710,",
+			"email": "aloisgeyer@gmx.at",
+			"last_update": "2022-03-02T07:07:31.000000Z",
+			"labelName": "customer",
+			"age": '',
+			"labelColor": "green",
+			"orgId": 942,
+			"bankaccount_info2": null
+		}, {
+			"id": 849,
+			"customer_id": "1475",
+			"name": "R1",
+			"address": "",
+			"email": "",
+			"last_update": "2022-03-01T12:29:01.000000Z",
+			"labelName": "customer",
+			"labelColor": "green",
+			"orgId": 936,
+			"age" :'',
+			"bankaccount_info2": null
+		}]
+	},
+	"code": 200
+}
+
+window.onload=insertdata();
+
+var input ,filter ;
+function searchdata(z){
+	
+	if(z === "customer_id"){
+		input=document.getElementsByClassName('customer_id');
+		rows1 = document.getElementsByTagName("tr");
+		for(let h=0; h<input.length;h++){
+			console.log(input[h]);
+			filter = input[h].value.toUpperCase();
+			console.log("filter",filter);
+			for(let i=0; i<rows1.length; i++){
+				console.log(filter, "--------filter--1");
+				td = rows1[i].getElementsByTagName('td')[1];
+		
+				if(td){
+					textvalue = td.textContent || td.innerText;
+		
+					console.log(filter,"--------filter--2");
+		
+					if (textvalue.toUpperCase().indexOf(filter) > -1) {
+						rows1[i].style.display = "";
+					}
+					else{
+						rows1[i].style.display = "none";
+					}
+				}
+			}
+		}
+	} else if(z === "name"){
+			input=document.getElementsByClassName('name');
+			rows1 = document.getElementsByTagName("tr");
+			for(let h=0; h<input.length;h++){
+				console.log(input[h]);
+				filter = input[h].value.toUpperCase();
+				console.log("filter",filter);
+				for(let i=0; i<rows1.length; i++){
+					console.log(filter, "--------filter--1");
+					td = rows1[i].getElementsByTagName('td')[1];
+			
+					if(td){
+						textvalue = td.textContent || td.innerText;
+			
+						console.log(filter,"--------filter--4");
+			
+						if (textvalue.toUpperCase().indexOf(filter) > -1) {
+							rows1[i].style.display = "";
+						}
+						else{
+							rows1[i].style.display = "none";
+						}
+					}
+				}
+			}
+	}else if(z === "address"){
+		input=document.getElementsByClassName('address');
+		rows1 = document.getElementsByTagName("tr");
+		for(let h=0; h<input.length;h++){
+			console.log(input[h]);
+			filter = input[h].value.toUpperCase();
+			console.log("filter",filter);
+			for(let i=0; i<rows1.length; i++){
+				console.log(filter, "--------filter--1");
+				td = rows1[i].getElementsByTagName('td')[1];
+		
+				if(td){
+					textvalue = td.textContent || td.innerText;
+		
+					console.log(filter,"--------filter--4");
+		
+					if (textvalue.toUpperCase().indexOf(filter) > -1) {
+						rows1[i].style.display = "";
+					}
+					else{
+						rows1[i].style.display = "none";
+					}
+				}
+			}
+		}
+}
+}
+
+function sortTable(data) {
+	console.log("column : ",data);
+	var  rows, switching, i, x, y, shouldSwitch;
+	// table = document.getElementById("myTable");
+	switching = true;
+	while (switching) {
+		switching = false;
+		rows = document.getElementsByTagName("tr");
+		console.log(rows,"rows");
+		for (i = 1; i < (rows.length - 1); i++) {
+			shouldSwitch = false;
+			console.log("column : ",data);
+		if(data=="Id"){
+			x = rows[i].getElementsByTagName("TD")[0];
+		y = rows[i + 1].getElementsByTagName("TD")[0];
+		if (!!x&& !!y&&x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+		  shouldSwitch = true;
+		  break;
+		}
+		}
+		else if(data=="Customer,Id"){
+			x = rows[i].getElementsByTagName("TD")[1];
+		    y = rows[i + 1].getElementsByTagName("TD")[1];
+		if (!!x&& !!y&&x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+		  shouldSwitch = true;
+		  break;
+		}
+
+		}
+		else if(data=="Name"){
+			x = rows[i].getElementsByTagName("TD")[2];
+		    y = rows[i + 1].getElementsByTagName("TD")[2];
+			console.log("x:",x);
+			console.log("y:",y);
+			if (!!x&& !!y&&x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+				shouldSwitch = true;
+				break;
+			}
+			
+		}  
+		else if(data=="Address"){
+			x = rows[i].getElementsByTagName("TD")[3];
+		    y = rows[i + 1].getElementsByTagName("TD")[3];
+			console.log("x:",x);
+			console.log("y", y);
+		if (!!x&& !!y&&x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+		  shouldSwitch = true;
+		  break;
+		}
+
+		}  
+		else if(data=="Email"){
+			x = rows[i].getElementsByTagName("TD")[4];
+		    y = rows[i + 1].getElementsByTagName("TD")[4];
+			console.log("x:",x);
+			console.log("y", y);
+		if (!!x&& !!y&&x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+		  shouldSwitch = true;
+		  break;
+		}
+
+		}  
+		else if(data=="Last,Update"){
+			x = rows[i].getElementsByTagName("TD")[5];
+		    y = rows[i + 1].getElementsByTagName("TD")[5];
+			console.log("x:",x);
+			console.log("y", y);
+		if (!!x&& !!y&&x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+		  shouldSwitch = true;
+		  break;
+		}
+
+		}  
+	  }
+	  if (shouldSwitch) {
+		rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+		console.log(rows[i]);
+		switching = true;
+	  }
+	}
+}
+
+function insertdata(){
+
+	let col=Object.keys(mydata.data.organizations[0]);
+	
+	console.log(col);
+
+	let table=document.createElement('table');
+
+	let tr=table.insertRow(-1);
+
+	for(let i=0;i<col.length;i++){
+		let th=document.createElement('th');
+		let x=col[i].split('_');
+		for(let m=0;m<x.length; m++){
+			x[m]=x[m].charAt(0).toUpperCase()+x[m].slice(1);
+			x.join('');
+		}
+		// console.log(x);
+        // th.innerHTML= x +  `<button onclick=sortTable('${x}')>sort</button> `;
+		th.innerHTML= x +  `<i class="fa fa-filter" aria-hidden="true" onclick=sortTable('${x}')></i> `;
+		// th.innerHTML= `<div class="header-flex"><p>${x}</p></div>` ;
+		tr.appendChild(th);
+	}
+
+	
+	let serch=mydata.data.searchableColumns;
+	let serchtable='';
+	tr =table.insertRow(-1);
+	for(let i=0;i<col.length;i++){
+		th=document.createElement('th');
+		let serchkey=col[i];
+		serchtable=serch.includes(serchkey) ? th.innerHTML=`<input onkeyup=searchdata("${col[i]}");  class="${col[i]}" type="text" placeholder=${col[i]}>`:'';
+		tr.appendChild(th);
+	}
+	for(let i=0;i<mydata.data.organizations.length;i++){
+		tr=table.insertRow(-1);
+
+		tr.setAttribute("class","rows")
+		for(let k=0;k<col.length;k++){
+			let  tablecell=tr.insertCell(-1);
+			tablecell.innerHTML=mydata.data.organizations[i][col[k]] || "null";
+			
+		}
+	}
+	// console.log(tr);
+   let showdata=document.getElementById('mytable');
+	showdata.innerHTML="";
+	showdata.appendChild(table);
+
+	let newkeyvalue=[];
+	for(let i=0;i<mydata.data.organizations.length;i++){
+		for(let key in mydata.data.organizations[i]){
+			// console.log(key); 
+			if(!(col.includes(key))){
+	
+				newkeyvalue.push(key);
+				// console.log(newkeyvalue);
+			}
+		}
+	}
+	var array = new Set(newkeyvalue);
+	// console.log(array);
+}
+
+function Capitalize(){
+	let i,frags =str.split('_');
+	for(let i=0;i < frags.length;i++){
+		frags[i]=frags[i].charAt(0).toUpperCase() + frags[i].slice(1);
+	}
+	return frags.join('');
+
+}
+function createnewtable(){
+	let col=Object.keys(mydata.data.organizations[0]);
+	// console.log(col);
+	let newvalue=[];
+	for(let i=0; i<mydata.data.organizations.length;i++){
+		for(let key in mydata.data.organizations[i]){
+			if((!col.includes(key))){
+				newvalue.push(key);
+			}
+		}
+	}
+	let value=new Set(newvalue);
+	let x=[...col ,...value];
+	// console.log(x);
+	// document.getElementById('heading').innerHTML=`Click It..! Get the New Key and Value is ${x}`;
+
+	let table=document.createElement('table');
+	let tr=table.insertRow(-1);
+	for(let i=0; i<x.length;i++){
+		// console.log(x.length);
+
+		let th=document.createElement("th");
+		th.innerHTML=x[i];
+		tr.appendChild(th);
+	}
+
+	for(let i=0;i<mydata.data.organizations.length;i++){
+		// console.log(mydata.data.organizations.length);
+tr=table.insertRow(-1);
+	for(let k=0;k<x.length;k++){
+		// console.log(x,"x value");
+		let  tablecell=tr.insertCell(-1);
+		tablecell.innerHTML=mydata.data.organizations[i][x[k]] || "null";
+
+		
+	}
+}
+	let showdata=document.getElementById('showtable');
+	showdata.innerHTML="";
+	showdata.appendChild(table);
+}
+
